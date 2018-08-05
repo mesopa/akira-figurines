b1216358119c0f4ec3ef6541d0cb416ca6549eb7
Pedro Serapio <pedro.mesopa@gmail.com>
2018-08-05 21:32:09 +0800

    Project Updated

---
+++ _dev/javascript/akira-figurines-javascript.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+  // Makes the parallax elements
+  function parallaxIt() {
+    // Create variables
+    var $fwindow = $(window);
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    var $backgrounds = [];
+
+    // For each of background parallax element
+    $('[data-type="background"]').each(function () {
+      var $backgroundObj = $(this);
+
+      $backgroundObj.__speed = ($backgroundObj.data('speed') || 1);
+      $backgroundObj.__fgOffset = $backgroundObj.offset().top;
+      $backgrounds.push($backgroundObj);
+    });
+
+    // Update positions
+    $fwindow.on('scroll resize', function () {
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+      $backgrounds.forEach(function ($backgroundObj) {
+        var yPos = ((scrollTop - $backgroundObj.__fgOffset) / $backgroundObj.__speed);
+
+        $backgroundObj.css({
+          backgroundPosition: 'center ' + yPos + 'px'
+        });
+      });
+    });
+
+    // Triggers window scroll for refresh
+    $fwindow.trigger('scroll');
+  };
+
+  parallaxIt();
+
+
+  // Smoth Scroll
+  $('a[href*=\\#]:not([href=\\#])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+      || location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 120
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+  // ==================
+  // Load Defered Fonts
+  // ==================
+  $("<link />", {
+    'rel': 'stylesheet',
+    'href': 'assets/css/akira-figurines-styles-fonts.min.css'
+  }).appendTo('head');
+});
